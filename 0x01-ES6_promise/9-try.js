ea0b6639bad8895c680b4c5665e962dca60a4404
simon teclai <nomistec.st@gmail.com>
2023-07-02 07:31:49 -0700

    9

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,11 @@
+  const queue = [];
+
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(err.toString());
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
